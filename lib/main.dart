d5e681e3d6435657e6b2d8bfab5983595b310199
tongnamuu <thban33@naver.com>
2025-11-22 03:54:41 +0900

    chapter1 style

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,14 @@ void main() {
     MaterialApp 은 항상 최상위 위치
     Scaffold는 바로 아래에 위치
      */
-    MaterialApp(home: Scaffold(
-      body: Center(
-        child: Text(
-          "hello flutter"
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+              child: Text("hello flutter", style: TextStyle(color: Colors.green,)
+            )
         ),
       ),
-    ),),
+    ),
   );
 }
